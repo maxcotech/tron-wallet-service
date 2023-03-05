973c7d1f8b8d7f97b862631abbc403c7576996d2
Chisom Maxwell <chiscomax2013@gmail.com>
2023-03-05 23:48:16 +0100

    add block status

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
+import AppService from "../services/AppService";
 import Controller from "./Controller";
 
 class HomeController extends Controller {
-    public static index(req: Request, res: Response){
+    public static async index(req: Request, res: Response){
+        const appService = new AppService();
         res.send({
             hello: "Hello Welcome to tron wallet ",
+            info: {
+                latestBlock: await appService.getLatestBlockNumber(),
+                consolidatedBlock: await appService.getLastIndexedNumber()
+            },
             baseUrl: req.baseUrl
           
         })
